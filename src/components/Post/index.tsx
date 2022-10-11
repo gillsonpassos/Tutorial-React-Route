e559f3bcfec6d0f9b5d851ea903ef1e953d8d89e
gillsonpassos <oliveirapassos07@outlook.com>
2022-10-11 11:00:27 -0300

    Usando Parametros

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useParams, useSearchParams } from "react-router-dom";
+import './style.css';
+export const Post = () => {
+  const params = useParams();
+  const { id } = params;
+  const [qs] = useSearchParams();
+
+    return (
+    <div>
+
+      <h1>Post {`Para: ${id}`} {`QS: ${qs}`}</h1>
+
+    </div>
+
+    );
+};
